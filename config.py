518f71ff62a48600f20e7ad284b53c9eb9f3653a
Sunjeet Kumar <sunjeetkasaudhan052@gmail.com>
2026-07-13 05:09:27 +0530

    Update config for Vercel

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+import os
 from flask_mysqldb import MySQL
 
 mysql = MySQL()
 
 class Config:
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "sunjeet@#ai123"
-    MYSQL_DB = "sers"
-    SECRET_KEY = "sers123"
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.getenv("MYSQL_DB", "sers")
+    SECRET_KEY = os.getenv("SECRET_KEY", "sers123")
 
 def init_db(app):
     app.config.from_object(Config)
