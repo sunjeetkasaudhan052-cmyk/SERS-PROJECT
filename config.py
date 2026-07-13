635c8e819adf4a7769974e1904542c0145500d53
Sunjeet Kumar <sunjeetkasaudhan052@gmail.com>
2026-07-13 10:36:49 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+import os
 import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-        port=4000,
-        user="otDZndFBCN8S6vm.root",
-        password="28hKV4MIop0jIYVi",
-        database="sys",
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT")),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
         ssl={"ssl": {}},
         cursorclass=pymysql.cursors.Cursor,
         autocommit=True
